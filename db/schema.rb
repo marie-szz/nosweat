e4f44abbeca09891d01ae1d8f2839bb78a1e36bf
Maxime Plasse <m20plasse@gmail.com>
2022-02-21 17:25:24 +0100

    MVC product

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_152031) do
+ActiveRecord::Schema.define(version: 2022_02_21_161244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "sport_category"
+    t.string "name"
+    t.integer "price"
+    t.string "description"
+    t.string "location"
+    t.string "quality"
+    t.string "photo_url"
+    t.boolean "delivery"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_02_21_152031) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
