e726b26f973818a93cc83ac324b9c590c6e856eb
Marie Sepanik <sepanik.marie@gmail.com>
2022-02-24 10:42:59 +0100

    add columns to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_162730) do
+ActiveRecord::Schema.define(version: 2022_02_24_094139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_162730) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "check_in"
     t.date "check_out"
+    t.string "status"
     t.index ["product_id"], name: "index_bookings_on_product_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -65,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_162730) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "size"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
